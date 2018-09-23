6978084f00e3dd96a4f91dd089ef1d80df780d22
David <Davidteri91@gmail.com>
2018-09-24 00:59:56 +0200

    qdMetaData: Add dependency on libhardware headers
    fixes errors in build with BOARD_VNDK_VERSION=current
    
    fatal error: 'hardware/gralloc1.h' file not found
                  ^~~~~~~~~~~~~~~~~~~~~
    1 error generated.
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -25,6 +25,7 @@ LOCAL_COPY_HEADERS              := qdMetaData.h qd_utils.h
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
 LOCAL_HEADER_LIBRARIES          := display_headers
+LOCAL_HEADER_LIBRARIES          += libhardware_headers
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
 LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
@@ -41,6 +42,7 @@ include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
 LOCAL_HEADER_LIBRARIES          := display_headers
+LOCAL_HEADER_LIBRARIES          += libhardware_headers
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
 LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
