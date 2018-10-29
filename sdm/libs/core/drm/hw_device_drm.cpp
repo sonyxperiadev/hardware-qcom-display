c0f64d107a523993f5a40a2d19d1bcdbfbfe2f5a
Alin Jerpelea <alin.jerpelea@sony.com>
2018-10-29 10:12:56 +0100

    Revert "sdm: Allow poweron before first commit."
    This reverts commit fbd58293b6ab7f8b94ab59a428e11dc7144b0fa3.

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -779,9 +779,7 @@ DisplayError HWDeviceDRM::PowerOn(const HWQosData &qos_data, int *release_fence)
   }
 
   if (first_cycle_) {
-    drm_atomic_intf_->Perform(DRMOps::CONNECTOR_SET_CRTC, token_.conn_id, token_.crtc_id);
-    drmModeModeInfo current_mode = connector_info_.modes[current_mode_index_].mode;
-    drm_atomic_intf_->Perform(DRMOps::CRTC_SET_MODE, token_.crtc_id, &current_mode);
+    return kErrorNone;
   }
 
   SetQOSData(qos_data);
