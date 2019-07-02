4078ee590d59d13f9a4fa8fff72436dc35a1cf7e
oshmoun <oshmoun100@googlemail.com>
2019-07-03 00:59:10 +0200

    account for gralloc/gralloc1 dependency switch
    the modules that depend on gralloc headers now need to know which set of headers to include.
    
    for bp modules, this is not possible due to not having a way of checking custom flags.
    this affects libqdutils.
    to work around this, define libqdutils.legacy that depends on gralloc1 headers.
    
    for makefile modules, check if TARGET_USES_GRALLOC1 and include the correct set of headers and dependencies.
    in the display hal, only hwc2 and gpu_tonemapper depend on gralloc headers and are defined in makefiles.
    
    Change-Id: Iff23d953911c84e2c1aa2de2799d0b8e507db09c

--- gpu_tonemapper/Android.mk
+++ gpu_tonemapper/Android.mk
@@ -12,6 +12,11 @@ LOCAL_MODULE              := libgpu_tonemapper
 LOCAL_VENDOR_MODULE       := true
 LOCAL_MODULE_TAGS         := optional
 LOCAL_HEADER_LIBRARIES    := display_headers
+ifeq ($(TARGET_USES_GRALLOC1), true)
+LOCAL_HEADER_LIBRARIES += display_intf_headers_gralloc1
+else
+LOCAL_HEADER_LIBRARIES += display_intf_headers_gralloc
+endif
 LOCAL_C_INCLUDES          += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_SHARED_LIBRARIES    := libEGL libGLESv2 libGLESv3 libui libutils liblog
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
