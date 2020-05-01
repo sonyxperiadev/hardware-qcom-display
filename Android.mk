c096a9b040af32a8c1e48ea41ca2d0b88ac12327
ix5 <ix5@users.noreply.github.com>
2020-05-02 00:49:42 +0200

    Android.mk: Organize for better readability
    Also account for platforms named `sdm` in addition to `msm`.

--- Android.mk
+++ Android.mk
@@ -1,23 +1,23 @@
 ifneq ($(TARGET_DISABLE_DISPLAY),true)
-sdm-libs := sdm/libs
-display-hals := include libdebug libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
 
-ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit libmemtrack hdmi_cec \
-                    $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils libdisplayconfig
-endif
+display-hals := \
+    include \
+    libdebug \
+    libqservice \
+    libqdutils \
+    sdm/libs/utils \
+    sdm/libs/core \
+    libcopybit \
+    libmemtrack \
+    hdmi_cec \
+    sdm/libs/hwc2 \
+    gpu_tonemapper \
+    libdrmutils \
+    libdisplayconfig
 
-ifeq ($(TARGET_USES_GRALLOC1), true)
+# All currently supported "legacy" devices use gralloc1
 display-hals += gralloc1
-else
-display-hals += gralloc
-endif
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
-endif
+include $(call all-named-subdir-makefiles,$(display-hals))
+
 endif #TARGET_DISABLE_DISPLAY
