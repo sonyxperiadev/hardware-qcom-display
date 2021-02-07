d065b80e1bf962973f3c0f744cff1f5f0c1eda4c
Marijn Suijten <marijns95@gmail.com>
2021-02-07 15:47:29 +0100

    Revert "composer: add support for display bandwidth limits management"
    This reverts commit b5ff0fb41c41a817d93941173b1aae888e9478bd.
    
    Bandwidth limit needs libthermalclient which is not available to us.

--- composer/hwc_display_builtin.h
+++ composer/hwc_display_builtin.h
@@ -30,7 +30,6 @@
 #ifndef __HWC_DISPLAY_BUILTIN_H__
 #define __HWC_DISPLAY_BUILTIN_H__
 
-#include <thermal_client.h>
 #include <mutex>
 #include <string>
 #include <vector>
@@ -143,7 +142,6 @@ class HWCDisplayBuiltIn : public HWCDisplay, public SyncTask<LayerStitchTaskCode
       int32_t samples_size[NUM_HISTOGRAM_COLOR_COMPONENTS],
       uint64_t *samples[NUM_HISTOGRAM_COLOR_COMPONENTS]);
   void Dump(std::ostringstream *os) override;
-  virtual HWC2::Error SetPowerMode(HWC2::PowerMode mode, bool teardown);
   virtual bool HasReadBackBufferSupport();
   virtual bool IsDisplayIdle();
 
@@ -172,8 +170,6 @@ class HWCDisplayBuiltIn : public HWCDisplay, public SyncTask<LayerStitchTaskCode
   bool AllocateStitchBuffer();
   void CacheAvrStatus();
   void PostCommitStitchLayers();
-  int GetBwCode(const DisplayConfigVariableInfo &attr);
-  void SetBwLimitHint(bool enable);
   void SetPartialUpdate(DisplayConfigFixedInfo fixed_info);
   uint32_t GetUpdatingAppLayersCount();
   void ValidateUiScaling();
@@ -181,10 +177,6 @@ class HWCDisplayBuiltIn : public HWCDisplay, public SyncTask<LayerStitchTaskCode
   // SyncTask methods.
   void OnTask(const LayerStitchTaskCode &task_code,
               SyncTask<LayerStitchTaskCode>::TaskContext *task_context);
-
-  constexpr static int kBwLow = 2;
-  constexpr static int kBwMedium = 3;
-  constexpr static int kBwHigh = 4;
 
   BufferAllocator *buffer_allocator_ = nullptr;
   CPUHint *cpu_hint_ = nullptr;
@@ -223,10 +215,6 @@ class HWCDisplayBuiltIn : public HWCDisplay, public SyncTask<LayerStitchTaskCode
   std::mutex sampling_mutex;
   bool api_sampling_vote = false;
   bool vndservice_sampling_vote = false;
-  int curr_refresh_rate_ = 0;
-  bool is_smart_panel_ = false;
-  const char *kDisplayBwName = "display_bw";
-  bool enable_bw_limits_ = false;
   bool disable_dyn_fps_ = false;
   bool enhance_idle_time_ = false;
   bool force_reset_validate_ = false;
